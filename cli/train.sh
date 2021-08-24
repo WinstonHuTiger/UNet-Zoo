faa6cd6600d276d6e21c657f6f5099bebf018bfc
WinstonHuTiger <2953620996@qq.com>
2021-08-24 23:09:11 +0800

    add training bash script

---
+++ cli/train.sh
@@ -0,0 +1,13 @@
+python train_model.py ./models/experiments/phiseg_rev_7_5_qubiq_brain_growth_4.py local dummy
+
+python train_model.py ./models/experiments/phiseg_rev_7_5_qubiq_brain_tumor1_4.py local dummy
+
+python train_model.py ./models/experiments/phiseg_rev_7_5_qubiq_brain_tumor2_4.py local dummy
+
+python train_model.py ./models/experiments/phiseg_rev_7_5_qubiq_brain_tumor3_4.py local dummy
+
+python train_model.py ./models/experiments/phiseg_rev_7_5_qubiq_kidney_4.py local dummy
+
+python train_model.py ./models/experiments/phiseg_rev_7_5_qubiq_prostate1_4.py local dummy
+
+python train_model.py ./models/experiments/phiseg_rev_7_5_qubiq_prostate2_growth_4.py local dummy
