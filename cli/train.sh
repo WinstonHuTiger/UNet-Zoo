b6f639ed8fbfbce4684e0e8f68c78f3ff7393446
WinstonHuTiger <2953620996@qq.com>
2021-08-24 23:10:30 +0800

    fix some typo

--- cli/train.sh
+++ cli/train.sh
@@ -10,4 +10,4 @@ python train_model.py ./models/experiments/phiseg_rev_7_5_qubiq_kidney_4.py loca
 
 python train_model.py ./models/experiments/phiseg_rev_7_5_qubiq_prostate1_4.py local dummy
 
-python train_model.py ./models/experiments/phiseg_rev_7_5_qubiq_prostate2_growth_4.py local dummy
+python train_model.py ./models/experiments/phiseg_rev_7_5_qubiq_prostate2_4.py local dummy
